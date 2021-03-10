9453f8ac1c5b6e94a427850535ae0a2531ba6644
joswinjohn <65504682+joswinjohn@users.noreply.github.com>
2021-03-09 20:15:12 -0500

    clicktest and js folder

---
+++ js/site.js
@@ -0,0 +1,26 @@
+﻿var clicked = 0;
+var time = 0;
+var timerstarted = false;
+
+function mouse1down() {
+    clicked += 1;
+    document.getElementById("clickmain").innerHTML = clicked;
+    if (timerstarted == false) {
+        var interval = setInterval(timeadd, 1000);
+    }
+    timerstarted = true;
+}
+
+function time1() {
+    document.getElementById("clicknum").innerHTML = clicked / 10 + " CPS";
+    document.getElementById("clickmain").disabled = true;
+}
+
+function timeadd() {
+    if (time >= 10) {
+        clearInterval(interval)
+    }
+
+    time += 1;
+    document.getElementById("clicknum").innerHTML = time;
+}
